cd2855614cc286d8a9a56df61c3f122f95b775fa
Artur de Moura <arturddmoura@Arturs-MacBook-Pro.local>
2022-04-04 19:27:55 -0300

    11-19

--- 150 Python Challenges/18.py
+++ 150 Python Challenges/18.py
@@ -1,1 +1,12 @@
 #Ask the user to enter a number. If it is under 10, display the message “Too low”, if their number is between 10 and 20, display “Correct”, otherwise display “Too high”.
+
+n1 = int(input("Please type a number: "))
+
+if n1 < 10:
+    print("Too low.")
+
+elif n1 >= 10 and n1 <= 20:
+    print("Correct.")
+
+else: 
+    print("Too high.")
--- 150 Python Challenges/19.py
+++ 150 Python Challenges/19.py
@@ -1,1 +1,12 @@
 #Ask the user to enter 1, 2 or 3. If they enter a 1, display the message “Thank you”, if they enter a 2, display “Well done”, if they enter a 3, display “Correct”. If they enter anything else, display “Error message”.
+
+number = int(input("Please enter 1, 2 or 3: "))
+
+if number == 1 or number == 3:
+    print("Correct.")
+
+elif number == 2:
+    print("Well done.")
+
+else:
+    print("Error!")
