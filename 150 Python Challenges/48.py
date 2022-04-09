275fbcaab86a4fa6bb12e52b23fbd71e94a9143e
Artur de Moura <arturddmoura@Arturs-MacBook-Pro.local>
2022-04-09 14:31:26 -0300

    45-51

--- 150 Python Challenges/48.py
+++ 150 Python Challenges/48.py
@@ -1,1 +1,16 @@
 #Ask for the name of somebody the user wants to invite to a party. After this, display the message “[name] has now been invited” and add 1 to the count. Then ask if they want to invite somebody else. Keep repeating this until they no longer want to invite anyone else to the party and then display how many people they have coming to the party.
+count = 0
+
+name = input("Who do you want to invite to the party? ")
+print(f"{name} has now been invited. ")
+count = count +1
+
+question = input("Do you want to invite anyone else? ")
+
+while question.upper() == "Y":
+    name = input("Who do you want to invite to the party? ")
+    print(f"{name} has now been invited. ")
+    count = count + 1
+    question = input("Do you want to invite anyone else? ")
+
+print(f"{count} people have been invited to the party.")
--- 150 Python Challenges/49.py
+++ 150 Python Challenges/49.py
@@ -1,1 +1,22 @@
 #Create a variable called compnum and set the value to 50. Ask the user to enter a number. While their guess is not the same as the compnum value, tell them if their guess is too low or too high and ask them to have another guess. If they enter the same value as compnum, display the message “Well done, you took [count] attempts”.
+
+compnum = 50
+count = 0
+
+user = int(input("Please enter a number: "))
+
+while user != compnum:
+    
+    if user > compnum:
+        print("Too high! ")
+        count = count + 1
+        user = int(input("Please enter a number: "))
+
+    
+    elif user < compnum:
+        print("Too low! ")
+        count = count + 1
+        user = int(input("Please enter a number: "))
+
+print(f"Good job, it took you {count} tries. ")
+    
