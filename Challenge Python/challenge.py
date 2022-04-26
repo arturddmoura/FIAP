360a261262bd6440eb26373b203ae807306c35c3
Artur de Moura <arturddmoura@Arturs-MacBook-Pro.local>
2022-04-26 15:19:01 -0300

    challenge1

---
+++ Challenge Python/challenge.py
@@ -0,0 +1,64 @@
+opções_validas = ["senha", "ajuda da senha", "número do telefone", "nomes", "email", "usuário"]
+lista_nomes = ["Adobe", "Apollo", "Canva", "PDL", "Hurb"]
+lista_ano = [10/2013, 7/2018, 5/2019, 10/2019, 3/2019]
+lista_vazados =["email, dica de senha, senha, usuário", "email, telefone, nome", 
+"email, nome, senha, usuário", "email, nome, telefone", "email, nome, senha, telefone"]
+lista_pontuação = [0, 0, 0, 0, 0]
+
+lista_sorted = []
+lista_resultados = []
+
+rodando = True
+
+while rodando == True:
+    menu_opção = int(input("O que deseja fazer?\n1) Adicionar ao banco de dados.\n2) Gerar lista organizada por gravidade.\n3) Parar programa.\nSelecione uma opção: "))
+
+    if menu_opção == 1:
+        nome = input("Qual é o nome da empresa que sofreu o vazamento? ")
+        lista_nomes.append(nome)
+        
+        mes = int(input("Qual foi o mês do vazamento? (Sem zeros à esquerda, por favor): "))
+        ano = int(input("Qual foi o ano do vazamento? (Com quatro dígitos): "))
+        lista_ano.append(mes/ano)
+
+        vazados = input("Quais informações foram vazadas? (As divida por vírgulas, por favor): ")
+        lista_vazados.append(vazados)
+
+        lista_pontuação.append(0)
+    
+    if menu_opção == 2:
+
+        for i in range (0, len(lista_nomes)):
+            if "senha" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 1000
+            if "ajuda de senha" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 750
+            if "usuário" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 500
+            if "nome" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 400
+            if "telefone" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 300
+            if "email" in lista_vazados[i]:
+                lista_pontuação[i] = lista_pontuação[i] + 200
+
+        for i in range (0, len(lista_ano)):
+            lista_ano[i] = float(lista_ano[i])
+            lista_pontuação[i] = lista_pontuação[i] + lista_ano[i]
+
+        print(lista_pontuação)
+
+        lista_sorted = sorted(lista_pontuação, reverse = True)
+
+        for i in range(0, len(lista_nomes)):
+            j = lista_pontuação.index(lista_sorted[i])
+            lista_resultados.append(lista_nomes[j])
+            lista_pontuação.remove(lista_sorted[i])
+            lista_nomes.remove(lista_nomes[j])
+
+        for i in range (0, len(lista_resultados)):
+            print (f"{i + 1} - {lista_resultados[i]}")
+    
+    if menu_opção == 3:
+        print("O programa será fechado. ")
+        rodando = False
---
+++ Challenge Python/challenge2.py
@@ -0,0 +1,25 @@
+'''
+
+
+lista_nomes = ["Appen", "Descomplica", "Mangafox", "Gravatar", "Lastfm", "MySpace", "Adobe", "Dropbox", "tumblr", "MyFitnessPal"]
+lista_ano = [2020, 2021, 2016, 2020, 2012, 2008, 2013, 2012, 2013, 2018]
+lista_vazados =["email, nome, senha, telefone", "email, nome, senha", "nascimento, email, usuário, senha", "email, nome, usuário",
+"email, nome, usuário", "email, usuário, senha", "email, dica de senha, usuário, senha", "email, senha", "email, senha", "email, usuário, senha"]
+lista_pontuação = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(0, len(Appen)):
+    if Appen[i] == "senha":
+        print(Appen[i])
+#fazer com pontuação
+
+Appen =  [2020, "email", "nome", "senha", "telefone"]
+Descomplica = [2021, "email", "nome", "senha"]
+MangaFox = [2016, "nascimento", "email", "usuário", "senha"]
+Gravatar = [2020, "email", "nome", "usuário"]
+Lastfm = [2012, "email", "senha", "usuário"]
+MySpace = [2008, "email", "usuário", "senha"]
+Adobe = [2013, "email", "dica de senha", "usuário", "senha"]
+Dropbox = [2012, "email", "senha"]
+tumblr = [2013, "email", "senha"]
+MyFitnessPal = [2018, "email", "usuário", "senha"]
+'''
