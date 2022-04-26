60834ea9a80709fe0fcd1415ee8749576293406f
Artur de Moura <arturddmoura@Arturs-MacBook-Pro.local>
2022-04-26 16:08:44 -0300

    corrigido

--- Challenge Python/challenge.py
+++ Challenge Python/challenge.py
@@ -1,7 +1,7 @@
 #opções_validas = ["senha", "ajuda da senha", "número do telefone", "nomes", "email", "usuário"]
 lista_nomes = ["Adobe", "Apollo", "Canva", "PDL", "Hurb"]
 lista_ano = [10/2013, 7/2018, 5/2019, 10/2019, 3/2019]
-lista_vazados =["email, dica de senha, senha, usuário", "email, telefone, nome", 
+lista_vazados = ["email, dica de senha, senha, usuário", "email, telefone, nome", 
 "email, nome, senha, usuário", "email, nome, telefone", "email, nome, senha, telefone"]
 lista_pontuação = [0, 0, 0, 0, 0]
 
@@ -12,7 +12,7 @@ rodando = True
 
 while rodando == True:
     menu_opção = int(input("O que deseja fazer?\n1) Adicionar ao banco de dados.\n2) Gerar lista organizada por gravidade.\n3) Parar programa.\nSelecione uma opção: "))
-
+    
     if menu_opção == 1:
         nome = input("Qual é o nome da empresa que sofreu o vazamento? ")
         lista_nomes.append(nome)
@@ -27,7 +27,6 @@ while rodando == True:
         lista_pontuação.append(0)
     
     if menu_opção == 2:
-
         for i in range (0, len(lista_nomes)):
             if "senha" in lista_vazados[i]:
                 lista_pontuação[i] = lista_pontuação[i] + 1000
